453213e5116affd9d4d5f86a138832f924eb59aa
kamausuzan197 <kamaususan197@gmail.com>
2022-10-05 10:06:59 +0300

    done

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ Bundler.require(*Rails.groups)
 
 module Phase4CookiesInRailsApi
   class Application < Rails::Application
+  include ActionController::Cookies
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
